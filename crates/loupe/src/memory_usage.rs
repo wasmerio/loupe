81f44703825d6ce5d474e65f49210b7e9b80c5a1
Ivan Enderlin <ivan@mnt.io>
2021-03-15 15:56:21 +0100

    feat: Implement `MemoryUsage` for `Result<T, E>`.

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -439,6 +439,34 @@ mod test_option_types {
     }
 }
 
+// Result types.
+impl<T: MemoryUsage, E: MemoryUsage> MemoryUsage for Result<T, E> {
+    fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
+        mem::size_of_val(self)
+            + match self.as_ref() {
+                Ok(value) => value.size_of_val(tracker),
+                Err(value) => value.size_of_val(tracker),
+            }
+    }
+}
+
+#[cfg(test)]
+mod test_result_types {
+    use super::*;
+
+    #[test]
+    fn test_result() {
+        let result: Result<i8, i16> = Err(2);
+        assert_size_of_val_eq!(result, 1 /* variant */ + 3 /* padding */ + 2 /* i16 */);
+
+        let result: Result<i8, i16> = Ok(1);
+        assert_size_of_val_eq!(result, 1 /* variant */ + 3 /* padding */ + 1 /* i8 */);
+
+        let result: Result<i32, ()> = Ok(1);
+        assert_size_of_val_eq!(result, 1 /* variant */ + 7 /* padding */ + 4 /* i32 */);
+    }
+}
+
 // TODO: Rc
 
 // TODO: Ref, RefCell, RefMut
