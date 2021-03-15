4d886a7e6d852ab13882be63007bb84db85016f7
Ivan Enderlin <ivan@mnt.io>
2021-03-15 16:13:37 +0100

    chore: Allow `assert_size_of_val_eq!` to have trailing commas.

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -44,7 +44,7 @@ impl MemoryUsage for () {
 
 #[cfg(test)]
 macro_rules! assert_size_of_val_eq {
-    ($value:expr, $expected:expr) => {
+    ($value:expr, $expected:expr $(,)*) => {
         assert_eq!(
             MemoryUsage::size_of_val(&$value, &mut BTreeSet::new()),
             $expected
@@ -327,7 +327,7 @@ mod test_tuple_types {
         let tuple: (i8, i16, i32) = (1, 2, 3);
         assert_size_of_val_eq!(
             tuple,
-            1 /* i8 */ + 2 /* i16 */ + 4 /* i32 */ + 1 /* padding */
+            1 /* i8 */ + 2 /* i16 */ + 4 /* i32 */ + 1, /* padding */
         );
 
         let tuple: (i32, i32) = (1, 2);
@@ -336,19 +336,19 @@ mod test_tuple_types {
         let tuple: (&str, &str) = ("", "");
         assert_size_of_val_eq!(
             tuple,
-            2 * POINTER_BYTE_SIZE + 1 * 0 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 0 /* str */
+            2 * POINTER_BYTE_SIZE + 1 * 0 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 0, /* str */
         );
 
         let tuple: (&str, &str) = ("a", "bc");
         assert_size_of_val_eq!(
             tuple,
-            2 * POINTER_BYTE_SIZE + 1 * 1 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 2 /* str */
+            2 * POINTER_BYTE_SIZE + 1 * 1 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 2, /* str */
         );
 
         let tuple: (&str, (i64, i64, i8)) = ("abc", (1, 2, 3));
         assert_size_of_val_eq!(
             tuple,
-            2 * POINTER_BYTE_SIZE + 1 * 3 /* str */ + 8 /* i64 */ + 8 /* i64 */ + 1 /* i8 */ + 7 /* padding */
+            2 * POINTER_BYTE_SIZE + 1 * 3 /* str */ + 8 /* i64 */ + 8 /* i64 */ + 1 /* i8 */ + 7, /* padding */
         );
     }
 }
@@ -380,7 +380,7 @@ mod test_box_types {
         let b: Box<(i8, i16)> = Box::new((1, 2));
         assert_size_of_val_eq!(
             b,
-            POINTER_BYTE_SIZE + 1 /* i8 */ + 2 /* i16 */ + 1 /* padding */
+            POINTER_BYTE_SIZE + 1 /* i8 */ + 2 /* i16 */ + 1, /* padding */
         );
     }
 }
@@ -420,7 +420,7 @@ mod test_option_types {
         let option: Option<&str> = Some("abc");
         assert_size_of_val_eq!(
             option,
-            1 /* variant */ + 15 /* padding */ + 2 * POINTER_BYTE_SIZE + 1 * 3 /* &str */
+            1 /* variant */ + 15 /* padding */ + 2 * POINTER_BYTE_SIZE + 1 * 3, /* &str */
         );
     }
 }
