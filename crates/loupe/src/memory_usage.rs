a4c568e8599f03fd66133ef53c93f18bcfec58b0
Ivan Enderlin <ivan@mnt.io>
2021-03-15 14:13:30 +0100

    feat: Implement `MemoryUsage` for `(T0, T1, …, T11)` (tuples).

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -55,7 +55,7 @@ macro_rules! impl_memory_usage_for_primitive {
         }
     };
 
-    ( $( $type:ty ),+ ) => {
+    ( $( $type:ty ),+ $(,)* ) => {
         $( impl_memory_usage_for_primitive!( $type ); )+
     }
 }
@@ -279,7 +279,72 @@ mod test_string_types {
     }
 }
 
-// TODO: tuples
+// Tuple types.
+macro_rules! impl_memory_usage_for_tuple {
+    ( $first_type:ident $(,)* ) => {};
+
+    ( $first_type:ident $( , $types:ident )+ $(,)* ) => {
+        impl< $first_type $( , $types )+ > MemoryUsage for ( $first_type $( , $types )+ )
+        where
+            $first_type: MemoryUsage,
+            $( $types: MemoryUsage ),*
+        {
+            fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
+                #[allow(non_snake_case)]
+                let ( $first_type $( , $types )+ ) = self;
+
+                mem::size_of_val(self)
+                    + $first_type.size_of_val(tracker) - mem::size_of_val($first_type)
+                    $( + $types.size_of_val(tracker) - mem::size_of_val($types) )+
+            }
+        }
+
+        impl_memory_usage_for_tuple!( $( $types ),+ );
+    };
+}
+
+impl_memory_usage_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L);
+
+#[cfg(test)]
+mod test_tuple_types {
+    use super::*;
+
+    #[test]
+    fn test_tuple() {
+        let tuple: (i8, i8) = (1, 2);
+        assert_size_of_val_eq!(tuple, 1 /* i8 */ + 1 /* i8 */);
+
+        let tuple: (i8, i16) = (1, 2);
+        assert_size_of_val_eq!(tuple, 1 /* i8 */ + 2 /* i16 */ + 1 /* padding */);
+
+        let tuple: (i8, i16, i32) = (1, 2, 3);
+        assert_size_of_val_eq!(
+            tuple,
+            1 /* i8 */ + 2 /* i16 */ + 4 /* i32 */ + 1 /* padding */
+        );
+
+        let tuple: (i32, i32) = (1, 2);
+        assert_size_of_val_eq!(tuple, 4 /* i32 */ + 4 /* i32 */);
+
+        let tuple: (&str, &str) = ("", "");
+        assert_size_of_val_eq!(
+            tuple,
+            2 * POINTER_BYTE_SIZE + 1 * 0 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 0 /* str */
+        );
+
+        let tuple: (&str, &str) = ("a", "bc");
+        assert_size_of_val_eq!(
+            tuple,
+            2 * POINTER_BYTE_SIZE + 1 * 1 /* str */ + 2 * POINTER_BYTE_SIZE + 1 * 2 /* str */
+        );
+
+        let tuple: (&str, (i64, i64, i8)) = ("abc", (1, 2, 3));
+        assert_size_of_val_eq!(
+            tuple,
+            2 * POINTER_BYTE_SIZE + 1 * 3 /* str */ + 8 /* i64 */ + 8 /* i64 */ + 1 /* i8 */ + 7 /* padding */
+        );
+    }
+}
 
 /// Standard library types
 
