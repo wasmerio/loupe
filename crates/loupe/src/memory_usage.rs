a9c3d7260280f8ae6a33e90b737908adf3e215cb
Ivan Enderlin <ivan@mnt.io>
2021-03-19 00:28:07 +0100

    feat: Implement `MemoryUsage` for `UnsafeCell`.

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -1,3 +1,4 @@
+use std::cell::UnsafeCell;
 #[cfg(test)]
 use std::collections::BTreeSet;
 use std::collections::HashMap;
@@ -735,6 +736,28 @@ mod test_path_types {
     }
 }
 
+// Cell types.
+impl<T> MemoryUsage for UnsafeCell<T> {
+    fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
+        if tracker.track(self.get() as *const ()) {
+            POINTER_BYTE_SIZE
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_cell_types {
+    use super::*;
+
+    #[test]
+    fn test_unsafecell() {
+        let cell = UnsafeCell::<i8>::new(1);
+        assert_size_of_val_eq!(cell, POINTER_BYTE_SIZE);
+    }
+}
+
 // TODO:
 //
 // * Cell
@@ -743,5 +766,4 @@ mod test_path_types {
 // * Ref
 // * RefCell
 // * RefMut
-// * UnsafeCell
 // * PhantomPinned
