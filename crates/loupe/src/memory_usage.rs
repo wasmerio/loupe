138b292d838c2c4ac42bab703e15d791c0eef71a
Ivan Enderlin <ivan@mnt.io>
2021-03-18 22:14:41 +0100

    feat: Implement `MemoryUsage` for `*const T` and `*mut T`.

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -49,10 +49,11 @@ impl MemoryUsage for () {
 #[cfg(test)]
 macro_rules! assert_size_of_val_eq {
     ($value:expr, $expected:expr $(,)*) => {
-        assert_eq!(
-            MemoryUsage::size_of_val(&$value, &mut BTreeSet::new()),
-            $expected
-        );
+        assert_size_of_val_eq!($value, $expected, &mut BTreeSet::new());
+    };
+
+    ($value:expr, $expected:expr, $tracker:expr $(,)*) => {
+        assert_eq!(MemoryUsage::size_of_val(&$value, $tracker), $expected);
     };
 }
 
@@ -110,9 +111,6 @@ mod test_primitive_types {
     );
 }
 
-// pointer
-// Pointers aren't necessarily safe to dereference, even if they're nonnull.
-
 // Reference types.
 impl<T: MemoryUsage> MemoryUsage for &T {
     fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
@@ -150,6 +148,56 @@ mod test_reference_types {
     fn test_mutable_reference() {
         assert_size_of_val_eq!(&mut 1i8, POINTER_BYTE_SIZE + 1);
         assert_size_of_val_eq!(&mut 1i64, POINTER_BYTE_SIZE + 8);
+    }
+}
+
+// Pointer types.
+impl<T> MemoryUsage for *const T {
+    fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
+        if tracker.track(*self as *const ()) {
+            POINTER_BYTE_SIZE
+        } else {
+            0
+        }
+    }
+}
+
+impl<T> MemoryUsage for *mut T {
+    fn size_of_val(&self, tracker: &mut dyn MemoryUsageTracker) -> usize {
+        if tracker.track(*self as *const _ as *const ()) {
+            POINTER_BYTE_SIZE
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_pointer_types {
+    use super::*;
+
+    #[test]
+    fn test_pointer() {
+        let mut tracker = BTreeSet::new();
+
+        let x = 1i8;
+        let ptr = &x as *const _;
+        assert_size_of_val_eq!(ptr, POINTER_BYTE_SIZE, &mut tracker);
+
+        let ptr = &x as *const _;
+        assert_size_of_val_eq!(ptr, 0, &mut tracker);
+    }
+
+    #[test]
+    fn test_mutable_pointer() {
+        let mut tracker = BTreeSet::new();
+
+        let mut x = 1i8;
+        let ptr = &mut x as *mut _;
+        assert_size_of_val_eq!(ptr, POINTER_BYTE_SIZE, &mut tracker);
+
+        let ptr = &mut x as *mut _;
+        assert_size_of_val_eq!(ptr, 0, &mut tracker);
     }
 }
 
