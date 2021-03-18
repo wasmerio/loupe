04fd0ac88f9fe62ce6b2c0c00cbfce04f672e27b
Ivan Enderlin <ivan@mnt.io>
2021-03-18 22:00:31 +0100

    feat: Implement `MemoryUsage` for `PathBuf`.

--- crates/loupe/src/memory_usage.rs
+++ crates/loupe/src/memory_usage.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeSet;
 use std::collections::HashMap;
 use std::marker::PhantomData;
 use std::mem;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex, RwLock};
 
 pub const POINTER_BYTE_SIZE: usize = if cfg!(target_pointer_width = "16") {
@@ -641,6 +642,30 @@ mod test_collection_types {
 impl<T> MemoryUsage for PhantomData<T> {
     fn size_of_val(&self, _: &mut dyn MemoryUsageTracker) -> usize {
         0
+    }
+}
+
+// Path types.
+impl MemoryUsage for PathBuf {
+    fn size_of_val(&self, _: &mut dyn MemoryUsageTracker) -> usize {
+        mem::size_of_val(self) + self.capacity()
+    }
+}
+
+#[cfg(test)]
+mod test_path_types {
+    use super::*;
+
+    #[test]
+    fn test_pathbuf() {
+        let mut path = PathBuf::new();
+        let empty_path_size = mem::size_of_val(&path);
+
+        path.push("foo");
+        assert_size_of_val_eq!(path, empty_path_size + 8);
+
+        path.push("foobar");
+        assert_size_of_val_eq!(path, empty_path_size + 16);
     }
 }
 
